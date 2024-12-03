3df6922310e357169c7a6ad6d0fc80f729688062
DarrenA-KingsIntHigh <Darren.Ackerman260@kingsinterhigh.co.uk>
2024-12-03 20:03:36 +0000

    Create databases.py

---
+++ databases.py
@@ -0,0 +1,24 @@
+import csv
+
+class Database():
+  database:dict
+  def __init__(self, filepath:str, feilds:list):
+    pass
+
+  def __del__(self):
+    pass
+    
+  def search(self):
+    pass
+
+  def add(self):
+    pass
+
+  def remove(self):
+    pass
+
+  def edit(self):
+    pass
+
+stock = Database("stock.csv",[])
+customers = Database("customers.csv",[])
